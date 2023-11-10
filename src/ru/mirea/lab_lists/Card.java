2d2d57ac2f54288867656a2320ccb09f207862c8
Chrnv1 <92212701+Chrnv1@users.noreply.github.com>
2023-11-10 10:52:25 +0300

    lab_lists

---
+++ src/ru/mirea/lab_lists/Card.java
@@ -0,0 +1,22 @@
+package ru.mirea.lab_lists;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Card {
+    private String name;
+    private int age;
+
+    public void readAttributes() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        name = scanner.nextLine();
+        System.out.print("Введите возраст: ");
+        age = scanner.nextInt();
+    }
+
+    public void displayAttributes() {
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+    }
+}
